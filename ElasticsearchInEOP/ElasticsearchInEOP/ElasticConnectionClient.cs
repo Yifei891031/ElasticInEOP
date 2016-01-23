be0269590840e64d8453e5760d9af89bb31ffc25
Yifei Li <lee891031@gmail.com>
2016-01-22 17:16:21 -0800

    Inplemented ElasticConnectionClient

--- ElasticsearchInEOP/ElasticsearchInEOP/ElasticConnectionClient.cs
+++ ElasticsearchInEOP/ElasticsearchInEOP/ElasticConnectionClient.cs
@@ -3,11 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nest;
 
 namespace ElasticsearchInEOP
 {
     class ElasticConnectionClient
     {
+        private static readonly ConnectionSettings _connectionSettings;
+
+        public static string IndexAlia { get { return "MessageEvent"; } }
+
+
+        public static Uri CreateUri(int port)
+        {
+            var host = "localhost";
+            return new Uri("http://" + host + ":" + port);
+        }
+
+        static ElasticConnectionClient()
+        {
+            _connectionSettings = new ConnectionSettings(CreateUri(9200)).SetDefaultIndex("MessageEvent");
+        }
+
+        public static ElasticClient GetClient()
+        {
+            return new ElasticClient(_connectionSettings);
+        }
+
+        public static string CreateIndexName()
+        {
+            return string.Format("{0}-{1:dd-MM-yyyy-HH-mm-ss}", IndexAlia, DateTime.UtcNow);
+        }
 
     }
 }
--- ElasticsearchInEOP/ElasticsearchInEOP/FeedPackage.cs
+++ ElasticsearchInEOP/ElasticsearchInEOP/FeedPackage.cs
@@ -5,6 +5,6 @@ public class FeedPackage
 {
 	public FeedPackage
 	{
-
+        public string 
 	}
 }
